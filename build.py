a8625d3f050cc22615ecf7161ea51f59bc1775f4
duckymomo360 <duckymomo360@gmail.com>
2023-03-08 19:39:57 -0800

    add build server

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 import os
-
-embedStr = "local EmbeddedModules = {\n"
-files = os.listdir("modules")
+import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 def readfile(path):
     file = open(path,"r")
@@ -9,20 +8,42 @@ def readfile(path):
     file.close()
     return str
 
-def addModuleFile(path):
+def build():
     global embedStr
+    embedStr = "local EmbeddedModules = {\n"
+    files = os.listdir("modules")
 
-    moduleName = os.path.splitext(os.path.basename(path))[0]
-    moduleSource = readfile(path)
+    def addModuleFile(path):
+        global embedStr
 
-    embedStr = embedStr + '["' + moduleName + '"] = function()\n' + moduleSource + '\nend,\n'
+        moduleName = os.path.splitext(os.path.basename(path))[0]
+        moduleSource = readfile(path)
 
-for filename in files:
-    addModuleFile("modules/" + filename)
+        embedStr = embedStr + '["' + moduleName + '"] = function()\n' + moduleSource + '\nend,\n'
 
-embedStr = embedStr + "}"
-embedStr = embedStr + "\n" + readfile("main.lua")
+    for filename in files:
+        addModuleFile("modules/" + filename)
 
-file = open("out.lua","w")
-file.write(embedStr)
-file.close()
+    embedStr = embedStr + "}"
+    embedStr = embedStr + "\n" + readfile("main.lua")
+
+    return embedStr
+
+if len(sys.argv) > 1 and sys.argv[1] == "server":
+    class BuildServer(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            message = bytes(build(), "utf8")
+            
+            self.send_header("Content-type", "text/plain; charset=utf-8")
+            self.send_header("Content-length", str(len(message)))
+            self.end_headers()
+
+            self.wfile.write(message)
+    
+    server = HTTPServer(("127.0.0.1", 8081), BuildServer)
+    server.serve_forever()
+else:
+    file = open("out.lua","w")
+    file.write(build())
+    file.close()
